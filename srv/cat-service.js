1b137a0ff096e116b04e1ea47a16dfe445f685e4
Jitendra Singh <jhonygautam@gmail.com>
2025-01-14 05:30:22 +0000

    Step 10 Completed

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,5 +1,5 @@
-module.exports = srv_func => {
-
+module.exports = (srv) => {
+// module.exports = srv_func => {
     // Reply mock data for Books............................................................................................
     // srv_func.on ('READ', 'Books', ()=>[
     //   { ID:202, title:'Wuthering Heights', author_ID:101, stock:12 },
@@ -11,5 +11,27 @@ module.exports = srv_func => {
     //   { ID:102, name:'Emily Brontë' },
     //   { ID:150, name:'Edgar Allen Poe' },
     //   { ID:170, name:'Richard Carpenter' }, ])
-   }
-   
+
+
+
+        const { Books } = cds.entities('my.bookshop')
+
+        // Reduce stock of ordered books
+        srv.before('CREATE', 'Orders', async (req) => {
+            const order = req.data
+            if (!order.amount || order.amount <= 0) return req.error(400, 'Order at least 1 book')
+            const tx = cds.transaction(req)
+            const affectedRows = await tx.run(
+                UPDATE(Books)
+                    .set({ stock: { '-=': order.amount } })
+                    .where({ stock: { '>=': order.amount },/*and*/ ID: order.book_ID })
+            )
+            if (affectedRows === 0) req.error(409, "Sold out, sorry")
+        })
+
+        // Add some discount for overstocked books
+        srv.after('READ', 'Books', each => {
+            if (each.stock > 111) each.title += ' -- 11% discount!'
+        })
+
+    } 
